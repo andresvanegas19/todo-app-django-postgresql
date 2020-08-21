b0b197fd4fca6a5e4eb3aab4a51166e7d145055c
andres reyes <andressantiagore@gmail.com>
2020-08-21 08:16:08 -0500

    Upload the files and finish the proyect

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is a todo app
+This app make an integration with postgresql and make the CRUD in the database
+The porpouse of this app is to keep track of which task you didn't do.
+
+![](todoapp.gif)
--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('list/', views.list_todo_items),
-    path('insert_todo_item', views.insert_todo_item, nam='insert')
+urlpatterns =[
+    path('list/',views.list_todo_items),
+    path('insert_todo/',views.insert_todo_item,name='insert_todo_item'),
+    path('delete_todo/<int:todo_id>/',views.delete_todo_item,name='delete_todo_item'),
 ]
