645c2c03a4fef3e1d851ec12d21e5f0ae1c84eba
andres reyes <andressantiagore@gmail.com>
2020-08-20 23:30:45 -0500

    add the html

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TodoDB',
+        'USER': 'andres',
+        'PASSWORD': '1',
+        'HOST':'localhost'
     }
 }
 
