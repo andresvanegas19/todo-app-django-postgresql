6ae1f1bc26d730b3bb91bea20cb1a94cc53c295c
andres reyes <andressantiagore@gmail.com>
2020-08-22 16:11:30 -0500

    uppload the ui and back end of the proyect and present into the readme

---
+++ crudempleey/README.md
@@ -0,0 +1,10 @@
+# Employee register
+This proyect is to dedicate a make a registion of employees
+This is how the postgresql look like
+![](database.png)
+This is how the page of the logging looks like
+![](logging.png)
+This is how the ui looks like
+![](UI.gif)
+This is how works
+![](works.gif)
--- crudempleey/employee_register/urls.py
+++ crudempleey/employee_register/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form), # Localhost/employee/list
-    path('list/', views.employee_list),
+    path('', views.employee_form, name="employee_insert"), # get and post request for inser
+    path('<int:id>/', views.employee_form, name="employee_update"), # get and post requesst for update
+    path('list/', views.employee_list, name="employee_list"), # this is for retrive and display records
+    path('delete/<int:id>', views.employee_delete, name="employee_delete"),
 ]
