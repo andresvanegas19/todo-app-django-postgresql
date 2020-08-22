935f0a917e04bb4a2021e86e6c00417829e27660
andres reyes <andressantiagore@gmail.com>
2020-08-22 00:11:58 -0500

    add the content but i need to fix it

--- crudempleey/employee_register/views.py
+++ crudempleey/employee_register/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EmployeeForm
+from .models import Employee
 
-# Create your views here.
+def employee_list(request):
+     return render(request, 'employee_register/employee_list.html')
+
+def employee_form(request):
+     form = EmployeeForm()
+     return render(request, 'employee_register/employee_form.html',{'form': form})
+
+def employee_delete(request):
+     return
+
